894e64f8fed6bc540558bcbe703e3d67f82dc1b2
HugoBzn <hugobazan1499@gmail.com>
2022-09-14 16:35:44 -0500

    Defining stores

---
+++ src/components/HomeStore.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Hi HomeStore</h1>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+import { useAuthStore } from "@/stores/auth";
+
+const authStore = useAuthStore();
+
+const store = useCounterStore();
+console.log(store.count);
+</script>
+
+<style scoped></style>
