9e5f8650a31030f18cdf8e84ab19fd8a4567038e
HugoBzn <hugobazan1499@gmail.com>
2022-09-14 17:10:47 -0500

    Get state from pinia store

---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <!-- Have count value -->
+    <p>Count value: {{ store.count }}</p>
+  </div>
+</template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+const store = useCounterStore();
+</script>
+
+<style scoped></style>
--- src/components/HomeStore.vue
+++ src/components/HomeStore.vue
@@ -1,17 +1,16 @@
 <template>
   <div>
     <h1>Hi HomeStore</h1>
+    <Counter />
   </div>
 </template>
 
 <script setup>
-import { useCounterStore } from "@/stores/counter";
+
 import { useAuthStore } from "@/stores/auth";
+import Counter from "@/components/Counter.vue";
 
 const authStore = useAuthStore();
-
-const store = useCounterStore();
-console.log(store.count);
 </script>
 
 <style scoped></style>
