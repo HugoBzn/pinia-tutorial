291e044cbf8fe06db6f8489d9771b57352e8b4d8
HugoBzn <hugobazan1499@gmail.com>
2022-09-14 18:07:47 -0500

    Need for getters

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,26 @@
 <template>
   <div>
+    <button @click="increment">Increment +</button> |
+    <button @click="decrement">Decrement -</button>
     <!-- Have count value -->
     <p>Count value: {{ store.count }}</p>
+    <br />
+    <p>Counter moved to {{ store.countDigitLength }} digit length</p>
   </div>
 </template>
 
 <script setup>
 import { useCounterStore } from "@/stores/counter";
+
 const store = useCounterStore();
+
+function increment() {
+  store.count++;
+}
+
+function decrement() {
+  store.count--;
+}
 </script>
 
 <style scoped></style>
--- src/components/HomeStore.vue
+++ src/components/HomeStore.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <h1>Hi HomeStore</h1>
+    <br />
     <Counter />
   </div>
 </template>
